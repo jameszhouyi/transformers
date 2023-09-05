a06119cfcc700684fe80490395167597213085a5
XumingGai <xuming.gai@intel.com>
2023-09-05 15:51:52 +0800

    add scripts and update requirements

---
+++ examples/flax/question-answering/run_qa.sh
@@ -0,0 +1,14 @@
+pip install -r ./requirements.txt
+export ZE_AFFINITY_MASK=3.0
+python run_qa.py   \
+--model_name_or_path bert-large-uncased-whole-word-masking   \
+--dataset_name squad   \
+--do_train   \
+--do_eval   \
+--per_device_train_batch_size 6   \
+--learning_rate 3e-5   \
+--num_train_epochs 2   \
+--max_seq_length 384   \
+--doc_stride 128   \
+--output_dir ./wwm_uncased_finetuned_squad/ \
+--eval_steps 1000
